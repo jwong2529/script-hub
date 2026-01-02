20cfb1da3bf48667e1f00e76895acd2fd65d295a
jwong2529 <janice.wong2529@gmail.com>
2026-01-02 15:00:47 -0800

    Initial Commit

---
+++ script-hub/script_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  script_hubApp.swift
+//  script-hub
+//
+//  Created by Janice Wong on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct script_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ script-hubTests/script_hubTests.swift
@@ -0,0 +1,17 @@
+//
+//  script_hubTests.swift
+//  script-hubTests
+//
+//  Created by Janice Wong on 1/2/26.
+//
+
+import Testing
+@testable import script_hub
+
+struct script_hubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
